12505ead9eac226aad4907d5ca6e86523a22b91a
jjuiddong <jjuiddong@hanmail.net>
2023-03-30 16:18:48 +0900

    update webrtc practice

--- README.md
+++ README.md
@@ -1,10 +1,44 @@
 # GStreamer-Practice
 GStreamer Practice with VS2019 C++
 
-- Src/
+- Server
 	- NodeJS + GStreamer video streaming example
+	-1. NodeJs Webserver wait to request
+	-2. Webbrowser Request, NodeJs Server send local camera streaming
 - Test1
 	- GStreamer simple example
 - gst-docs-discontinued-for-monorepo
 	- GStreamer tutorials
+- gstwebrtc-demos-master
+	- https://github.com/centricular/gstwebrtc-demos
+	- webrtc signalling server example
+		- simple_server.py
+			- need python3
+			- pip install websockets
+			- remove 1 line code: #!/usr/bin/env python3
+				- error occure
+			- generate.sh cannot create cert.pem
+				- run bash
+				- run 
+					- openssl req -x509 -newkey rsa:4096 -keyout ${OUTDIR}key.pem -out ${OUTDIR}cert.pem -days 365 -nodes
+					- copy key.pem, cert.pem from user directory to project directory
+
+- WebRTCTest
+	- https://github.com/sampleref/gstreamer-cpp-example
+
+- WebRTCTest2
+	- reference source code
+		- https://github.com/GStreamer/gstreamer/blob/main/subprojects/gst-examples/webrtc/sendrecv/gst/webrtc-sendrecv.c
+	- send, receive binary data with WebRTC channel
+
+- WebRTCRecvOnly
+	- reference code
+		- https://github.com/GStreamer/gstreamer/blob/main/subprojects/gst-examples/webrtc/sendonly/webrtc-recvonly-h264.c
+	- 
+
+- WebRTCSendOnly
+	- https://github.com/GStreamer/gstreamer/blob/main/subprojects/gst-examples/webrtc/sendonly/webrtc-unidirectional-h264.c
+	- webrtc-unidirectional-h264.cp
+	- open http server
+		- when connect browser, send camera streaming from server camera
 
--- Src/Server/main.js
+++ Src/Server/main.js
@@ -7,11 +7,14 @@ var child = require('child_process');
 require('log-timestamp');   //adds timestamp in console.log()
 
 var app = express();
-app.use(cors())
+// app.use(cors())
 app.use(express.static(__dirname + '/'));
 
 var httpServer = http.createServer(app);
 const port = 9001;  //change port number is required
+var gstMuxer = undefined
+var errCnt = 0
+var clientCnt = 0
 
 //send the html page which holds the video tag
 app.get('/', function (req, res) {
@@ -39,7 +42,21 @@ app.get('/stream', function (req, res) {
 
     var tcpServer = net.createServer(function (socket) {
         socket.on('data', function (data) {
-            res.write(data);
+            const result = res.write(data);
+            if (result == false) {
+                if (++errCnt > 2) {
+                    // console.log('write fail')
+                    // res.end()
+                    // close web browser
+                    if (gstMuxer) {
+                        // console.log('kill gstreamer')
+                        // gstMuxer.kill()
+                        // gstMuxer = undefined
+                    }
+                }
+            } else {
+                errCnt = 0
+            }
         });
         socket.on('close', function (had_error) {
             console.log('Socket closed.');
@@ -53,18 +70,22 @@ app.get('/stream', function (req, res) {
         console.log("Connection started.");
         if (gstMuxer == undefined) {
             console.log("inside gstMuxer == undefined");
-            var cmd = 'gst-launch-1.0';
-            var args = getGstPipelineArguments(this);
-            var gstMuxer = child.spawn(cmd, args);
-
-            gstMuxer.stderr.on('data', onSpawnError);
-            gstMuxer.on('exit', onSpawnExit);
-
+            // setTimeout(() => {
+                var cmd = 'gst-launch-1.0';
+                var args = getGstPipelineArguments(this);
+                gstMuxer = child.spawn(cmd, args);
+                errCnt = 0
+    
+                gstMuxer.stderr.on('data', onSpawnError);
+                gstMuxer.on('exit', onSpawnExit);
+                
+            // }, 1000);
         }
         else {
             console.log("New GST pipeline rejected because gstMuxer != undefined.");
         }
     });
+
 });
 
 httpServer.listen(port);
